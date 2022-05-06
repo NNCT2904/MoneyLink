073cfb7b0a461d3fc7d93d2775d4490c698a5107
JiatengTao <taojia@deakin.edu.au>
2022-05-06 16:09:27 +1000

    update

--- moneylink-backend/src/app.js
+++ moneylink-backend/src/app.js
@@ -7,11 +7,11 @@ const mongoConnect = require('./mongoConnect');
 const PORT = 3001;
 
 const app = express();
-
+app.use(cors());
 app.use('/api', appRouter);
 
 app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3002");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   next();
