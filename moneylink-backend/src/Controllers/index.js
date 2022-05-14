db7b93e810dca6f8c8b211bdf7719d42b1502c10
ThanhNguyen <nnct2000@gmail.com>
2022-05-15 03:04:51 +1000

    Add signup/login mock-up functions and their test cases

--- moneylink-backend/src/Controllers/index.js
+++ moneylink-backend/src/Controllers/index.js
@@ -1,8 +1,10 @@
+const Authentication = require('./Authentication');
 const HouseholdController = require('./HouseholdController');
 const BillController = require('./BillController');
 const UserController = require('./UserController');
 
 module.exports = {
+  Authentication,
   HouseholdController,
   BillController,
   UserController,
