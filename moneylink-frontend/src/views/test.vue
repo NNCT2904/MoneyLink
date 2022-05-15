78858e38366393b3a6ffa01a54c9a72e7247b9e4
ZHEHAO ZHOU <zhouzheh@deakin.edu.au>
2022-05-16 07:33:08 +1000

    components added

--- moneylink-frontend/src/views/test.vue
+++ moneylink-frontend/src/views/test.vue
@@ -1,39 +1,30 @@
 <template>
+<el-container class="layout-container-demo" style="height: 500px">
+    <el-aside width="200px">
     <Aside/>
+    </el-aside>
+    <el-main>
+        <Testcard />
+        <Balance />
+    </el-main>
     
-    <el-button @click="openByParentMethod ">打开</el-button>
-
-    <test-dialog  ref="testDom"  v-model:visible="flag" ></test-dialog>
-
+  </el-container>
 </template>
 
 
 
 <script>
 import Aside from "@/components/Aside";
-import TestDialog from "@/components/TestDialog"
-import { reactive, toRefs, ref, onMounted, watch } from "vue"
+import Testcard from "@/components/TestCard.vue"
 
+import Balance from "@/components/Balance.vue"
 
-export default {
-    setup() {
-     const testDom = ref(null)
-     const openByParentMethod = () => {
-        testDom.value.dialogVisble = true
-        }
-    const closeByParentMethod= () => {
-      testDom.value.dialogVisble = false
-        }
-    return {
-      testDom,
-      openByParentMethod,
-      closeByParentMethod,
-        }
-    },
+export default { 
     name: 'test',
     components: {
     Aside,
-    TestDialog,
+    Testcard,
+    Balance
 },
     data(){
         
@@ -43,5 +34,7 @@ export default {
 }
 </script>
 <style>
-
+.el-main {
+  padding: 0;
+}
 </style>
