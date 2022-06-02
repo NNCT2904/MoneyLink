195f84ece037f422aa991b43848eaafeb5ce1aae
JiatengTao <taojia@deakin.edu.au>
2022-06-02 19:08:24 +1000

    update header style

--- moneylink-frontend/src/components/Header.vue
+++ moneylink-frontend/src/components/Header.vue
@@ -3,15 +3,13 @@
     <img alt="Vue logo" src="../assets/logo.jpg">
     <div style="width: 200px; padding-left: 10px; font-weight: bold; ">MoneyLink</div>
     <div style="flex: 1"></div>
-    <div v-if="this.$store.state.ifFrameShow" style="width: 100px" >
+    <div v-if="this.$store.state.ifFrameShow" style="width: 300px" >
         <el-dropdown>
             <span class="el-dropdown-link">
-           <br>Hi, {{headerData.name}} <el-icon class="el-icon--right"><arrow-down /></el-icon>
+           <br>Household: {{headerData.name}} <el-icon class="el-icon--right"><arrow-down /></el-icon>
             </span>
         <template #dropdown>
             <el-dropdown-menu>
-            <el-dropdown-item>Profile</el-dropdown-item>
-            <el-dropdown-item>Settings</el-dropdown-item>
             <el-dropdown-item @click="logOut()">Log out</el-dropdown-item>
         </el-dropdown-menu>
     </template>
